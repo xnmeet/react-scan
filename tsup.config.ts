d6b14adcb6cfd6ab2af53d3ecad57b14a20ca777
Aiden Bai <aiden.bai05@gmail.com>
2024-11-19 04:18:26 -0800

    refactor(tsup.config): Change minify option to 'terser'

--- tsup.config.ts
+++ tsup.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   platform: 'browser',
   treeshake: true,
   dts: true,
-  minify: false,
+  minify: 'terser',
   env: {
     NODE_ENV: process.env.NODE_ENV ?? 'development',
   },
