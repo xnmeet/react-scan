8d9e84f06e8dd7cafb46f735fa952e44788ab106
Nisarg Patel <awesomenisarg@gmail.com>
2024-12-03 17:27:57 -0800

    refactor(performance): Enhance performance monitoring by filtering interactions and updating entry types

--- packages/scan/src/core/monitor/performance.ts
+++ packages/scan/src/core/monitor/performance.ts
@@ -9,24 +9,33 @@ export function initPerformanceMonitoring() {
     const monitor = Store.monitor.value;
     if (!monitor) return;
 
-    list.getEntries().forEach((entry) => {
-      if (entry.entryType === 'interaction') {
-        const eventTiming = entry as PerformanceEventTiming;
-        const interaction: Interaction = {
-          id: generateId(),
-          name: eventTiming.name,
-          type: eventTiming.name,
-          time: eventTiming.duration,
-          timestamp: eventTiming.startTime,
-        };
+    const entries = list.getEntries();
+    const interactionIds = new Set<number>();
+    for (let i = 0, len = entries.length; i < len; i++) {
+      const entry = entries[i] as PerformanceEventTiming & {
+        interactionId?: number;
+      };
+      // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+      if (!(entry.interactionId || entry.entryType === 'first-input')) continue;
+      else if (entry.interactionId && interactionIds.has(entry.interactionId))
+        continue;
+      interactionIds.add(entry.interactionId!);
 
-        monitor.interactions.push(interaction);
-      }
-    });
+      const interaction: Interaction = {
+        id: generateId(),
+        name: entry.name,
+        type: entry.name,
+        time: entry.duration,
+        timestamp: entry.startTime,
+      };
+
+      monitor.interactions.push(interaction);
+    }
   });
 
   observer.observe({
-    entryTypes: ['interaction'],
+    entryTypes: ['event', 'first-input'],
+    buffered: true,
   });
 
   return observer;
