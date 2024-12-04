0b9e69ac88cc7b6a64d793cacbd8666f82185f67
Aiden Bai <aiden.bai05@gmail.com>
2024-12-04 06:55:15 -0800

    refactor(app): Update import statements in page.tsx

--- packages/website/app/monitoring/page.tsx
+++ packages/website/app/monitoring/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Link from 'next/link';
 import { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import Waitlist from './(components)/waitlist';
