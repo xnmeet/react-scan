17335fbd043b0f0980b3cde2b2a90e9fb9376afa
Nisarg Patel <awesomenisarg@gmail.com>
2024-12-06 03:23:15 -0800

    fix: issues

--- packages/scan/src/core/monitor/index.ts
+++ packages/scan/src/core/monitor/index.ts
@@ -3,23 +3,60 @@ import React from 'react';
 import { scan, Store } from '..';
 import { initPerformanceMonitoring } from './performance';
 import { getSession } from './utils';
+import { computeRoute } from './params/utils';
 
-export const Monitoring = ({
+export const BaseMonitor = ({
   url,
   apiKey,
   path,
   route,
 }: { url?: string; apiKey: string } & {
+  path: string;
   route: string | null;
+}) => {
+  if (!apiKey)
+    throw new Error('Please provide a valid API key for React Scan monitoring');
+  url ??= 'https://monitoring.react-scan.com/api/v1/ingest';
+
+  Store.monitor.value ??= {
+    // components: new Map(),
+    pendingRequests: 0,
+    url,
+    apiKey,
+    interactions: [],
+    session: getSession(),
+    route,
+    path,
+  };
+  Store.monitor.value.route = route;
+  Store.monitor.value.path = path;
+
+  React.useEffect(() => {
+    scan({
+      enabled: true,
+      showToolbar: false,
+    });
+    return initPerformanceMonitoring();
+  }, []);
+
+  return null;
+};
+
+export const Monitoring = ({
+  url,
+  apiKey,
+  params,
+  path,
+}: { url?: string; apiKey: string } & {
+  params: Record<string, string | Array<string>>;
   path: string;
 }) => {
   if (!apiKey)
     throw new Error('Please provide a valid API key for React Scan monitoring');
-
-  // TODO(nisarg): Fix this default value after we confirm the URL
   url ??= 'https://monitoring.react-scan.com/api/v1/ingest';
+
+  const route = computeRoute(path, params);
   Store.monitor.value ??= {
-    // components: new Map(),
     pendingRequests: 0,
     url,
     apiKey,
