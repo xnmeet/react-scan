b5d205cd7c94c3a75c616eca054a74da33754288
Aiden Bai <aiden.bai05@gmail.com>
2024-12-12 00:46:30 -0800

    refactor(scan): Move LICENSE and update banner in tsup.config.ts

--- packages/scan/tsup.config.ts
+++ packages/scan/tsup.config.ts
@@ -27,10 +27,32 @@ const addDirectivesToChunkFiles = async (readPath: string): Promise<void> => {
   }
 };
 
+const banner = `/**
+ * Copyright 2024 Aiden Bai, Million Software, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the “Software”), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */`;
+
 export default defineConfig([
   {
     entry: ['./src/auto.ts'],
     outDir: DIST_PATH,
+    banner: {
+      js: banner,
+    },
     splitting: false,
     clean: false,
     sourcemap: false,
@@ -65,6 +87,9 @@ export default defineConfig([
       './src/core/monitor/params/remix.ts',
       './src/core/monitor/params/astro/component.ts',
     ],
+    banner: {
+      js: banner,
+    },
     outDir: DIST_PATH,
     splitting: false,
     clean: true,
