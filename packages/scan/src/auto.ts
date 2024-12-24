d42ae058765754648c7b07cfd86e3a5127d06318
RobPruzan <rapruzan@buffalo.edu>
2024-12-23 22:56:33 -0500

    chore: bump to 0.0.53

--- packages/scan/src/auto.ts
+++ packages/scan/src/auto.ts
@@ -2,9 +2,7 @@ import 'bippy'; // implicit init RDT hook
 import { scan } from './index';
 
 if (typeof window !== 'undefined') {
-  scan({
-    dangerouslyForceRunInProduction: true,
-  });
+  scan();
   window.reactScan = scan;
 }
 
