3dfa9bc815e60accea1a9362651e09be49598bcc
Pavel Ivanov <pafelka@gmail.com>
2025-01-09 04:39:49 +0200

    fix: enable dangerouslyForceRunInProduction (#185)

--- packages/scan/src/auto.ts
+++ packages/scan/src/auto.ts
@@ -2,7 +2,7 @@ import 'bippy'; // implicit init RDT hook
 import { scan } from './index';
 
 if (typeof window !== 'undefined') {
-  scan();
+  scan({ dangerouslyForceRunInProduction: true });
   window.reactScan = scan;
 }
 
