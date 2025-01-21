41b3e9cb9627c8020344466c5a6a532d2044cc9f
Nisarg Patel <awesomenisarg@gmail.com>
2025-01-21 12:10:46 -0800

    fix: monitoring page

--- packages/website/app/monitoring/page.tsx
+++ packages/website/app/monitoring/page.tsx
@@ -16,6 +16,7 @@ export default function Monitoring() {
 
             <p>It takes less than 2 minutes to set up.</p>
 
+
             <div className="flex gap-4">
               <Link
                 className="inline-flex items-center justify-center h-[38px] px-4 py-2 text-sm font-medium text-white rounded-md transition-all duration-200 ease-out bg-scan-600 hover:bg-scan-500 border border-scan-500 hover:border-scan-400 focus-visible:outline-4 focus-visible:outline-offset-1 focus-visible:outline-scan-400"
@@ -32,18 +33,18 @@ export default function Monitoring() {
               </Link>
             </div>
 
-            <div className="flex flex-wrap gap-4 text-gray-400 text-sm mt-2">
+            <div className="flex flex-wrap gap-4 text-gray-500 text-sm mt-2">
               <div className="flex items-center gap-2">
                 <span>✓</span>
                 <span>Quick setup</span>
               </div>
               <div className="flex items-center gap-2">
                 <span>✓</span>
-                <span>Free 30-day trial</span>
+                <span>No obligation</span>
               </div>
               <div className="flex items-center gap-2">
                 <span>✓</span>
-                <span>No obligation</span>
+                <span>30-day free trial</span>
               </div>
             </div>
           </div>
@@ -70,6 +71,21 @@ export default function Monitoring() {
               className="absolute inset-0 w-full h-full"
             />
           </div>
+        </div>
+        {/* View full page link */}
+        <div className="mt-4 text-center">
+        
+          <Link
+            href="https://dashboard.react-scan.com/project/demo"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex items-center gap-2 text-sm text-black  transition-colors"
+          >
+            This is a preview. <span className="text-scan-600 hover:text-scan-700 transition-colors ">View full page</span>
+            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M3.5 3C3.5 2.72386 3.72386 2.5 4 2.5H8.5C8.77614 2.5 9 2.72386 9 3V7.5C9 7.77614 8.77614 8 8.5 8C8.22386 8 8 7.77614 8 7.5V4.20711L3.85355 8.35355C3.65829 8.54882 3.34171 8.54882 3.14645 8.35355C2.95118 8.15829 2.95118 7.84171 3.14645 7.64645L7.29289 3.5H4C3.72386 3.5 3.5 3.27614 3.5 3Z" fill="currentColor" className="text-scan-600 hover:text-scan-700 transition-colors"/>
+            </svg>
+          </Link>
         </div>
       </div>
     </div>
