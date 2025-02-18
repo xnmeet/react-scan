8f4c21aefc0d7e582cd9140d97a0ba99e4df218d
Mahidhar Kakumani <43433128+ursmahi@users.noreply.github.com>
2025-02-18 19:25:36 +0530

    make copyright year dynamic in footer (#262)

--- packages/website/components/footer.tsx
+++ packages/website/components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="flex-1 mt-auto border-t border-t-white/20 bg-black p-6 text-xs">
       <div className="mx-auto flex w-full max-w-xl flex-wrap items-center justify-between gap-2 text-neutral-100">
         <div className="flex gap-2">
-          <div>© 2024 Million Software, Inc.</div>
+          <div>© {new Date().getFullYear()} Million Software, Inc.</div>
         </div>
         <div>
           {' '}
