2512a6e442f06b955f0d5c0920e84e2dc2397880
RobPruzan <rapruzan@buffalo.edu>
2025-02-25 15:29:43 -0500

    chore: add react component name to website

--- packages/website/next.config.ts
+++ packages/website/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import ReactComponentNamePlugin from "react-scan/react-component-name/webpack";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.plugins.push(ReactComponentNamePlugin({}))
+    return config
+  }
 };
 
 export default nextConfig;
