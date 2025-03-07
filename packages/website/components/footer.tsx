613d6eede55d1a7a96ec7dbdae4e2a6e8f54b88e
RobPruzan <rapruzan@buffalo.edu>
2025-03-06 22:24:30 -0500

    fix: temporary footer color so toolbar is visible

--- packages/website/components/footer.tsx
+++ packages/website/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="flex-1 mt-auto border-t border-t-white/20 bg-black p-6 text-xs">
+    <div className="flex-1 mt-auto border-t border-t-white/20 bg-[#252526] p-6 text-xs">
       <div className="mx-auto flex w-full max-w-xl flex-wrap items-center justify-between gap-2 text-neutral-100">
         <div className="flex gap-2">
           <div>© {new Date().getFullYear()} Million Software, Inc.</div>
